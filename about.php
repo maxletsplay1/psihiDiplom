e505f0f59c55c9fdf130a711e31f0a3fa6e468ad
maxletsplay1 <105488018+maxletsplay1@users.noreply.github.com>
2023-11-26 02:49:01 +0700

    хуй

--- about.php
+++ about.php
@@ -13,6 +13,16 @@
         flex-direction: row;
         justify-content: space-between;
     }
+
+    @media screen and (max-width: 991px) {
+        .container {
+            display: flex;
+            flex-wrap: wrap;
+            flex-direction: row;
+            justify-content: center;
+        }
+
+    }
 </style>
 
 
@@ -22,18 +32,96 @@ require "header.php"
 <p class="word-wrap text-center fs-1 mt-4 py-0 text-uppercase">наши сотрудники</p>
 <div class="container px-0">
     <div class="card m-4" style="max-width: 340px;">
-        <img src="/media/slide3.jpg" class="card-img-top" alt="...">
+        <img src="/media/personal/fotoshef1.jpg" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Чел</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 class="card-title">ГЕРШЕНОВИЧ ГРИГОРИЙ
+                МАРКЕЛОВИЧ</h5>
+            <p class="card-text">главный врач, врач-организатор,
+                врач-психиатр высшей категории</p>
         </div>
         <ul class="list-group list-group-flush">
-            <li class="list-group-item">An item</li>
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
+            <li class="list-group-item">Телефон: Телефон: (391) 246-25-98</li>
+            <li class="list-group-item">Факс: (391) 246-25-98</li>
+            <li class="list-group-item">Е-mail: kkpnd1@kraspsixo.ru</li>
         </ul>
     </div>
 
+
+    <div class="card m-4" style="max-width: 340px;">
+        <img src="/media/personal/jukovskaya1.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">ЖУКОВСКАЯ ГАЛИНА ПЕТРОВНА</h5>
+            <p class="card-text">заместитель главного врача по лечебной работе,
+                врач-психиатр высшей категории</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Телефон: Телефон: (391) 246-25-98</li>
+            <li class="list-group-item">Факс: (391) 246-25-98</li>
+            <li class="list-group-item">Е-mail: kkpnd1@kraspsixo.ru</li>
+        </ul>
+    </div>
+
+
+    <div class="card m-4" style="max-width: 340px;">
+        <img src="/media/personal/potehina1.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">ПОТЕХИНА НАТАЛИЯ АРКАДЬЕВНА</h5>
+            <p class="card-text">Главная медицинская сестра, менеджер в
+                системе здравоохранения</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Телефон: Телефон: (391) 246-25-98</li>
+            <li class="list-group-item">Факс: (391) 246-25-98</li>
+            <li class="list-group-item">Е-mail: kkpnd1@kraspsixo.ru</li>
+        </ul>
+    </div>
+
+
+    <div class="card m-4" style="max-width: 340px;">
+        <img src="/media/personal/katseval1.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">КАЦЕВАЛ АЛЕКСАНДР ВАСИЛЬЕВИЧ</h5>
+            <p class="card-text">заместитель главного врача по
+                организационно–методической работе,
+                врач-психиатр высшей категории</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Телефон: Телефон: (391) 246-25-98</li>
+            <li class="list-group-item">Факс: (391) 246-25-98</li>
+            <li class="list-group-item">Е-mail: kkpnd1@kraspsixo.ru</li>
+        </ul>
+    </div>
+
+
+    <div class="card m-4" style="max-width: 340px;">
+        <img src="/media/personal/davidova1.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">ДАВЫДОВА МАРИНА ВИКТОРОВНА</h5>
+            <p class="card-text">заместитель главного врача по
+                амбулаторно-поликлинической работе,
+                врач-психиатр высшей категории</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Телефон: Телефон: (391) 246-25-98</li>
+            <li class="list-group-item">Факс: (391) 246-25-98</li>
+            <li class="list-group-item">Е-mail: kkpnd1@kraspsixo.ru</li>
+        </ul>
+    </div>
+
+
+    <div class="card m-4" style="max-width: 340px;">
+        <img src="/media/personal/petrov1.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">ПЕТРОВ АНАТОЛИЙ СТАНИСЛАВОВИЧ</h5>
+            <p class="card-text">заместитель главного врача по
+                судебно-психиатрической экспертизе</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Телефон: Телефон: (391) 246-25-98</li>
+            <li class="list-group-item">Факс: (391) 246-25-98</li>
+            <li class="list-group-item">Е-mail: kkpnd1@kraspsixo.ru</li>
+        </ul>
+    </div>
 </div>
 <?
 require "footer.php"
--- contacts.php
+++ contacts.php
@@ -14,6 +14,16 @@
         flex-wrap: wrap;
     }
 
+    @media screen and (max-width: 1191px) {
+        #first {
+            display: flex;
+            justify-content: center;
+            flex-direction: row;
+            flex-wrap: wrap;
+        }
+
+    }
+
     #card {
         margin-right: 20px;
         margin-left: 20px;
@@ -26,19 +36,20 @@
 </style>
 <? require "header.php" ?>
 <p class="word-wrap text-center fs-1 mt-4 py-0 text-uppercase">наши филиалы</p>
+
 <div class="first" id="first">
 
     <div class="card my-4" id="card" style="max-width: 540px;">
         <div class="row g-0">
             <div class="col-md-4">
-                <img src="media/slide1.jpg" id="img" class="img-fluid rounded" alt="Card title">
+                <img src="media/filialy/kansk1.jpeg" id="img" class="img-fluid rounded" alt="Card title">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                    <p class="bold fs-4">Пример</p>
+                    <p class="bold fs-4">Филиал №1</p>
                     <p class="card-text fs-6">Понедельник-пятница: с 8:00 до 16:00.<br>
                         Суббота, воскресенье: выходной</p>
-                    <p class="card-text fs-6"><small class="text-muted">г. Канск, ул. Революции, 37</small></p>
+                    <p class="card-text fs-6">г. Канск, ул. Революции, 37</p>
                 </div>
             </div>
         </div>
@@ -46,32 +57,32 @@
     <div class="card my-4" id="card" style="max-width: 540px;">
         <div class="row g-0">
             <div class="col-md-4">
-                <img src="media/slide1.jpg" id="img" class="img-fluid rounded" alt="Card title">
+                <img src="media/filialy/les1.jpeg" id="img" class="img-fluid rounded" alt="Card title">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                    <p class="bold fs-4">Пример</p>
+                    <p class="bold fs-4">Филиал №2</p>
                     <p class="card-text fs-6">Понедельник-пятница: с 8:00 до 16:00.<br>
                         Суббота, воскресенье: выходной</p>
-                    <p class="card-text fs-6"><small class="text-muted">г. Канск, ул. Революции, 37</small></p>
+                    <p class="card-text fs-6">г. Лесосибирск, ул. Громовой, 24</p>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<div class="first" id="first">
+
+
 
     <div class="card my-4" id="card" style="max-width: 540px;">
         <div class="row g-0">
             <div class="col-md-4">
-                <img src="media/slide1.jpg" id="img" class="img-fluid rounded" alt="Card title">
+                <img src="media/filialy/minus1.jpeg" id="img" class="img-fluid rounded" alt="Card title">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                    <p class="bold fs-4">Пример</p>
+                    <p class="bold fs-4">Филиал №3</p>
                     <p class="card-text fs-6">Понедельник-пятница: с 8:00 до 16:00.<br>
                         Суббота, воскресенье: выходной</p>
-                    <p class="card-text fs-6"><small class="text-muted">г. Канск, ул. Революции, 37</small></p>
+                    <p class="card-text fs-6">г. Минусинск, ул. Советская, 39В</p>
                 </div>
             </div>
         </div>
@@ -79,17 +90,18 @@
     <div class="card my-4" id="card" style="max-width: 540px;">
         <div class="row g-0">
             <div class="col-md-4">
-                <img src="media/slide1.jpg" id="img" class="img-fluid rounded" alt="Card title">
+                <img src="media/filialy/ach1.jpeg" id="img" class="img-fluid rounded" alt="Card title">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                    <p class="bold fs-4">Пример</p>
+                    <p class="bold fs-4">Филиал №4</p>
                     <p class="card-text fs-6">Понедельник-пятница: с 8:00 до 16:00.<br>
                         Суббота, воскресенье: выходной</p>
-                    <p class="card-text fs-6"><small class="text-muted">г. Канск, ул. Революции, 37</small></p>
+                    <p class="card-text fs-6">г.Ачинск, ул.Гагарина, 6</p>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 <? require "footer.php" ?>
