8d65c7dd9aa2961be7952f93eb6ca0d9aa0be836
maxletsplay1 <105488018+maxletsplay1@users.noreply.github.com>
2024-01-19 15:04:31 +0700

    йоу

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ require "header.php"
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <!-- <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button> -->
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active" data-bs-interval="10000">
@@ -41,12 +41,12 @@ require "header.php"
         <h2>забота</h2>
       </div>
     </div>
-    <div class="carousel-item">
+    <!-- <div class="carousel-item">
       <img src="/media/prof1.jpg" style="min-height: 400px; object-fit: cover;" class="d-block w-100 rounded" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h2>профессионализм</h2>
       </div>
-    </div>
+    </div> -->
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
